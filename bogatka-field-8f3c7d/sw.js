06b32267924dac9389ab8aed21cc4ea18885b679
AFK Work <noclickafk@gmail.com>
2026-06-24 21:49:16 +0300

    Refresh Bogatka offline cache for version 1.1

--- bogatka-field-8f3c7d/sw.js
+++ bogatka-field-8f3c7d/sw.js
@@ -1,4 +1,4 @@
-const CACHE='bogatka-location-v1';
+const CACHE='bogatka-location-v2';
 const ASSETS=['./','./index.html','./style.css','./config.js','./ui.js','./actions.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
