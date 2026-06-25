3a694b437cbe39b9188e75b37c45ea9698b5902d
AFK Work <noclickafk@gmail.com>
2026-06-25 13:35:33 +0300

    Add Bogatka Supabase client configuration

---
+++ bogatka-field-8f3c7d/supabase-config.js
@@ -0,0 +1,6 @@
+window.BOGATKA_SUPABASE = Object.freeze({
+  url: "https://fascqulqvlxmraktuxez.supabase.co",
+  publishableKey: "sb_publishable_mGXqGjOMke-m24a_wsZ1OA_Gp8Nyeqm",
+  projectRef: "fascqulqvlxmraktuxez",
+  photoBucket: "bogatka-photos"
+});
