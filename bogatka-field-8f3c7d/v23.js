4fbcc8acc15dd767da11c5e029c559cfe646d6df
AFK Work <noclickafk@gmail.com>
2026-06-26 22:37:27 +0300

    Load v412 decision panel assets

--- bogatka-field-8f3c7d/v23.js
+++ bogatka-field-8f3c7d/v23.js
@@ -49,6 +49,7 @@ function applyVersion23Enhancements(){
   loadBogatkaPatch('link',{rel:'stylesheet',href:'./compare-v340.css'});
   loadBogatkaPatch('link',{rel:'stylesheet',href:'./suite-v400.css'});
   loadBogatkaPatch('link',{rel:'stylesheet',href:'./visual-v411.css'});
+  loadBogatkaPatch('link',{rel:'stylesheet',href:'./decision-panel-v412.css'});
   loadBogatkaPatch('script',{src:'./auth-v31.js'});
   loadBogatkaPatch('script',{src:'./auth-signup-fix-v31.js'});
   loadBogatkaPatch('script',{src:'./members-v32.js'});
@@ -69,6 +70,7 @@ function applyVersion23Enhancements(){
   loadBogatkaPatch('script',{src:'./report-v400.js'});
   loadBogatkaPatch('script',{src:'./access-version-v400.js'});
   loadBogatkaPatch('script',{src:'./visual-v411.js'});
+  loadBogatkaPatch('script',{src:'./decision-panel-v412.js'});
 
   document.addEventListener('keydown',event=>{
     const target=event.target;
