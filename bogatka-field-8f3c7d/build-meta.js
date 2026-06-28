a27ef24c3dd71f5ef4210c7b99ce93ac434a10bf
AFK Work <noclickafk@gmail.com>
2026-06-28 19:58:16 +0300

    feat(versioning): add generated build metadata source

---
+++ bogatka-field-8f3c7d/build-meta.js
@@ -0,0 +1,12 @@
+(function(scope){
+  'use strict';
+
+  const build=Object.freeze({
+    version:'4.2.5',
+    versionToken:'425',
+    sourceCommit:'d3d86f22ce9d260b07efa8550594038537871e52',
+    builtAt:'2026-06-28T16:58:06Z',
+  });
+
+  scope.BOGATKA_BUILD=build;
+})(typeof self!=='undefined'?self:globalThis);
